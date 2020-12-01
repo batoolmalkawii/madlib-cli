edf0fd25c833dc798cd1b6450bc6cd996c8c03e3
Batool Malkawi <batoolmalkawii@gmail.com>
2020-12-01 16:39:22 +0200

    Done

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,45 @@
+import re
+
+"""
+read_template function that takes in a path to text file and returns a stripped string of the file’s contents.
+"""
+def read_template(file_path):
+    with open(file_path, "r") as file:
+        text = file.read()
+    return(text)
+
+"""
+parse function that takes in a template string and returns a string with language parts removed,
+and a separate list of those language parts.
+"""
+def parse(text):
+    placeholders = re.findall(r"\{(.*?)\}", text)
+    words = re.sub(r"\{(.*?)\}", "$", text)
+    return words, placeholders
+
+"""
+merge function that takes in a “bare” template and a list of user entered language parts, 
+and returns a string with the language parts inserted into the template.
+"""
+def  merge(words, user_words):
+    for word in user_words:
+        words = words.replace("$", word, 1)
+    return(words)
+
+
+if __name__ == "__main__":
+    text = (read_template("assets/text.txt"))
+    words, placeholders = parse(text)
+    print("**************************************")
+    print("**          THIS IS MADLIB          **")
+    print("**************************************")
+    print("--            LET'S PLAY!           --")
+    print("--------------------------------------")
+    user_words = []
+    for placeholder in placeholders:
+        user_words.append(input(f"Enter a {placeholder}: "))
+    game_result = merge(words, user_words)
+    print("********YOUR MADLIBS says:************")
+    print(game_result)
+
+  
